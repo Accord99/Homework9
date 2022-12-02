082257950b6c86d6294738e324328d2cf81f4453
Accord99 <114819430+Accord99@users.noreply.github.com>
2022-12-02 10:44:24 +0300

    решили задачу 64

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Решаем все задачи через рекурсию!
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+
+//N = 5 -> "4, 2"
+//N = 8 -> "8, 6, 4, 2"
+
+Console.Clear();
+
+Console.WriteLine("Введите любое натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void ReverseOrderNaturalNumber(int n)
+{
+    if(n == 0) return;
+    Console.Write($"{n}, ");
+    ReverseOrderNaturalNumber(n - 1);
+}
+
+ReverseOrderNaturalNumber(number);
